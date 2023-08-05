e6dc5eb094dcded50c930d066402302244e88cf4
João Paulo <jpaulo_sanchez@hotmail.com>
2023-08-05 15:38:43 -0300

    Add initial GameStages and GameActions

--- main.go
+++ main.go
@@ -15,4 +15,10 @@ func run(d display.Displayer) {
 	board.LoadInitialBoard()
 
 	d.Display(board)
+
+	gm := game.NewGameManager(game.TeamBlack, &board)
+
+	for gm.Turns = 0; gm.States.GameFinished; gm.Turns++ {
+
+	}
 }
